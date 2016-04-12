985391b3a10f381472caa3d377b7510416ab7086
Adam Kenneweg <adrock.1996@hotmail.com>
2016-04-12 17:11:57 -0400

    package v4

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.listen(process.env.PORT || 8080);
