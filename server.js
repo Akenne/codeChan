f9e6c0c15ed10f4939c22241235cd08e3c425825
Adam Kenneweg <adrock.1996@hotmail.com>
2016-04-12 17:14:52 -0400

    cors1

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.use(express.static(__dirname + '/'));
 
 app.listen(process.env.PORT || 8080);
